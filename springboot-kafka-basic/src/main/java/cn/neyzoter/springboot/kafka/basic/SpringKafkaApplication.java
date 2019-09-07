00942ec1dfac54b34933d723869137dc34ef22a6
Neyzoter <sonechaochao@gmail.com>
2019-09-07 20:58:32 +0800

    del log4j2

--- springboot-kafka-basic/src/main/java/cn/neyzoter/springboot/kafka/basic/SpringKafkaApplication.java
+++ springboot-kafka-basic/src/main/java/cn/neyzoter/springboot/kafka/basic/SpringKafkaApplication.java
@@ -1,7 +1,5 @@
 package cn.neyzoter.springboot.kafka.basic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
