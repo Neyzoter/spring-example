981dadcc8edb0d0caf944fb3bd5cb86fe1e75436
Neyzoter <sonechaochao@gmail.com>
2019-09-12 14:55:30 +0800

    update

---
+++ springboot-kafka-serializer/src/test/java/cn/neyzoter/springboot/kafka/serializer/TestSpringbootApplication.java
@@ -0,0 +1,17 @@
+package cn.neyzoter.springboot.kafka.serializer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test SpringbootApplication
+ * @author Neyzoter Song
+ * @date 2019/9/12
+ */
+@RunWith(SpringRunner.class)
+public class TestSpringbootApplication {
+    @Test
+    public void contextLoads() {
+    }
+}
