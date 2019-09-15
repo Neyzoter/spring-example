5f88c7e57c4e42b434d8c1c1ac7c3ec5b202e220
Neyzoter <sonechaochao@gmail.com>
2019-09-15 15:02:05 +0800

    update

--- springboot-RestTemplate/src/main/java/cn/neyzoter/springboot/rest/task/Taskjobs.java
+++ springboot-RestTemplate/src/main/java/cn/neyzoter/springboot/rest/task/Taskjobs.java
@@ -2,8 +2,13 @@ package cn.neyzoter.springboot.rest.task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Task jobs
@@ -13,7 +18,41 @@ import org.springframework.stereotype.Component;
 @Component("taskJob")
 public class Taskjobs {
     private final static Logger logger = LoggerFactory.getLogger(Taskjobs.class);
-    private int counter = 0;
+    private Integer counter = 0;
+
+    /**
+     * Organization
+     */
+    private String org = "zju";
+    /**
+     * InfluxDB bucket
+     */
+    private String bucket = "influxdb_bucket";
+    /**
+     * precision
+     */
+    private String precision = "s";
+    /**
+     * token
+     */
+    private String token = "yzwAKztIXZLJNSvTPeUuFW7P9z4oWd_NLnGZNcIuoJMY7PCZEm1Lu1s-IIjloYFiSBVhRss7aMaDbh58WdlhGA==";
+    /**
+     * Measurement
+     */
+    private String measurement = "vehicle";
+    /**
+     * tag set
+     */
+    private String tags = "vehicleId=100";
+    /**
+     * field set
+     */
+    private String fields = "vehicleSpeed=";
+
+    /**
+     * timestamp
+     */
+    private String timestamp = counter.toString();
 
     /**
      * post msg to InfluxDB periodically
@@ -22,9 +61,25 @@ public class Taskjobs {
     public void post2InfluxDb(){
         try{
             logger.info("Starting post : "+counter);
+            RestTemplate restTemplate = new RestTemplate();
+            String url = String.format("http://localhost:9999/api/v2/write?org=%s&bucket=%s&precision=%s", org, bucket, precision);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            headers.add("Authorization", "Token "+token);
+            String lineProtoBody = this.measurement+","+this.tags+" "+this.fields+Math.sin((double) this.counter);
 
+            HttpEntity<String> request = new HttpEntity<>(lineProtoBody, headers);
+            ResponseEntity<String> response = restTemplate.postForEntity( url, request , String.class );
+            logger.info("url : "+url);
+            logger.info("response : "+ response);
         }catch (Exception e){
             logger.error("",e);
+        }finally {
+            this.counter += 5;
+            this.timestamp = counter.toString();
+            if(counter >= Integer.MAX_VALUE){
+                counter = 0;
+            }
         }
     }
 }
