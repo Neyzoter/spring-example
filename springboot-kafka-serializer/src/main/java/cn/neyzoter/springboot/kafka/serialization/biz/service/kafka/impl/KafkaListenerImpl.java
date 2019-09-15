79afc09939c8c12b04b0c146bd5e7a252f56362f
Neyzoter <sonechaochao@gmail.com>
2019-09-16 00:10:42 +0800

    repaire

--- springboot-kafka-serializer/src/main/java/cn/neyzoter/springboot/kafka/serialization/biz/service/kafka/impl/KafkaListenerImpl.java
+++ springboot-kafka-serializer/src/main/java/cn/neyzoter/springboot/kafka/serialization/biz/service/kafka/impl/KafkaListenerImpl.java
@@ -3,7 +3,6 @@ package cn.neyzoter.springboot.kafka.serialization.biz.service.kafka.impl;
 import cn.neyzoter.springboot.kafka.serialization.biz.domain.vehicle.VehicleHttpPack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
