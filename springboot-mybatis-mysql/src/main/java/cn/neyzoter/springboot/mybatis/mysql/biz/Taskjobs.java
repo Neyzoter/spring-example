65480d7bfcdb0b031ca9fd5084cfc100219e959e
Neyzoter <sonechaochao@gmail.com>
2019-09-20 06:09:56 +0800

    update

--- springboot-mybatis-mysql/src/main/java/cn/neyzoter/springboot/mybatis/mysql/biz/Taskjobs.java
+++ springboot-mybatis-mysql/src/main/java/cn/neyzoter/springboot/mybatis/mysql/biz/Taskjobs.java
@@ -1,11 +1,11 @@
 package cn.neyzoter.springboot.mybatis.mysql.biz;
 
+import cn.neyzoter.springboot.mybatis.mysql.biz.service.impl.UserServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /**
  * Task jobs
@@ -15,11 +15,13 @@ import javax.annotation.Resource;
 @Component("taskjobs")
 public class Taskjobs {
     private final static Logger logger = LoggerFactory.getLogger(Taskjobs.class);
+    @Autowired
+    UserServiceImpl userService;
     /**
      * save user info periodically
      */
     @Scheduled(cron="10/5 * * * * ?")
     public void saveUser(){
-        logger.info(userMapper.selectAll().toString());
+        logger.info(userService.findAllUser().toString());
     }
 }
